9db6a40c73c09c0d0c59ac04dfda7c1390a26e12
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2022-05-15 18:59:15 +0300

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -1,11 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract MyContract {
+/*
+Author: Panagiotis Doupidis
+A.M.: 89
+Date: 15/05/22
+
+-- Aristotle University of Thessaloniki
+-- Data & Web Science MSc Program
+-- Decentralized Technologies Course
+=========================================
+-- Assignment 2, 2022 - Ethereum
+*/
+
+contract GovDapp {
+    // Array to store 3 VAT levels specified (24%, 13%, 6%)
     uint8[3] public taxes;
+    // List of goverment controlled addresses for each level
     address[3] public govAddresses;
+    // Array to store accumulated VAT for each level
     uint256[3] public gatheredVat;
 
+    /* 
+    Store proceeds for each person/address that receives
+    funds as an associative array mapping TaxID's to
+    Wei's
+    */
     mapping(uint256 => uint256) private proceedPerId;
 
     struct VatLevels {
@@ -14,51 +34,86 @@ contract MyContract {
         uint8 low;
     }
 
+    // To store the person/address with the most proceeds
     struct MaxProceeds {
         uint256 id;
         address addr;
     }
 
+    // The owner that deploys the contract
     address public owner;
     VatLevels public vatLevels = VatLevels(24, 13, 6);
+
+    /* 
+    Declare struct to store information about the
+    recipient with the most proceeds, make sure it's
+    private so that a getter method is not exposed
+    */
     MaxProceeds private s_maxProceeds;
     uint256 private maxProceeds;
-    uint256 public constant MAX_NONVAT = 50000000000000000;
 
+    // Max amount that VAT doesn't apply to (0.05 ETH -> Wei)
+    uint256 public constant MAX_NONVAT = 50000000000000000;
+    uint16 public constant MAX_SENTENCE_LENGTH = 80;
+
+    // Define constructor that accepts 3 gov. addresses
     constructor(
         address gov1,
         address gov2,
         address gov3
     ) {
+        // Owner of the contract is the one deploying it
         owner = msg.sender;
+        // Initialize proceeds
         maxProceeds = 0;
 
+        // Initialize each of the 3 arrays defined with values
         taxes = [vatLevels.high, vatLevels.mid, vatLevels.low];
         govAddresses = [gov1, gov2, gov3];
         gatheredVat = [0, 0, 0];
     }
 
+    /*
+    Function modifier to restrict access only to the owner
+    of the contract
+    */
     modifier onlyOwner() {
         require(msg.sender == owner, "Unauthorized access");
         _;
     }
 
+    /*
+    Function modifier that constraints the amount of a transaction
+    to a specific value (upper bound)
+    */
     modifier costs(uint256 amount) {
         require(msg.value <= amount, "Exceeded max limit.");
         _;
     }
 
+    // Initialize logging events for each overloaded function
     event LogMsg1(address to, uint256 amount);
     event LogMsg2(address to, uint256 amount, uint8 vatLevel);
     event LogMsg3(address to, uint256 amount, uint8 vatLevel, string comment);
 
+    /*
+    Transfers funds from one address to another only if the amount specified
+    is <= 0.05 ETH
+    */
     function sendFunds(address destination) public payable costs(MAX_NONVAT) {
+        // Make sure sender has the funds
         require(checkBalance(msg.sender) > msg.value, "Insufficient funds");
 
+        // Transfer the funds without taking VAT into account
         payable(destination).transfer(msg.value);
         emit LogMsg1(destination, msg.value);
     }
 
+    /* 
+    Transfer funds from one address to another given that all prerequisites
+    are met, meaning that the sender has the needed funds and a valid VAT level
+    is passed as argument
+    */
     function sendFunds(
         address destination,
         uint256 taxId,
@@ -67,23 +122,37 @@ contract MyContract {
         require(checkIndexValidity(idx), "Invalid index, [0, 1, 2] available");
         require(checkBalance(msg.sender) > msg.value, "Insufficient funds");
 
+        // Calculate the VAT based on the index argument
         uint256 tax = (msg.value * taxes[idx]) / 100;
 
+        // First, transfer part of the funds to destination address
         payable(destination).transfer(msg.value - tax);
+        /* 
+        Then, make a tax payment to the goverment controlled address
+        for that level
+        */
         payable(govAddresses[idx]).transfer(tax);
 
+        // Update the proceeds of recipient with the current amount - tax
         proceedPerId[taxId] += msg.value - tax;
+        // Update the total VAT for that level
         gatheredVat[idx] += tax;
 
+        /*
+        Check if the user with the most proceeds has changed
+        after this transaction and update the struct accordingly.
+        */
         if (proceedPerId[taxId] > maxProceeds) {
             s_maxProceeds.id = taxId;
             s_maxProceeds.addr = destination;
             maxProceeds = proceedPerId[taxId];
         }
 
+        // Emit event if transaction is successful
         emit LogMsg2(destination, msg.value, idx);
     }
 
+    // Same as function above with the added functionality of adding comment
     function sendFunds(
         address destination,
         uint256 taxId,
@@ -92,7 +161,11 @@ contract MyContract {
     ) public payable {
         require(checkIndexValidity(idx), "Invalid index, [0, 1, 2] available");
         require(checkBalance(msg.sender) > msg.value, "Insufficient funds");
-        require(utfStringLength(comment) <= 80, "Comment > 80 characters");
+        // Make sure the comment is not over the character limit
+        require(
+            utfStringLength(comment) <= MAX_SENTENCE_LENGTH,
+            "Comment >80 characters"
+        );
 
         uint256 tax = (msg.value * taxes[idx]) / 100;
 
@@ -111,6 +184,7 @@ contract MyContract {
         emit LogMsg3(destination, msg.value, idx, comment);
     }
 
+    // Function that returns the length of a string
     function utfStringLength(string memory str)
         internal
         pure
@@ -130,23 +204,31 @@ contract MyContract {
         }
     }
 
+    // Returns VAT for each level specified, available to everyone
     function getVatForLevel(uint8 level) public view returns (uint256) {
         assert(checkIndexValidity(level));
         return gatheredVat[level];
     }
 
+    // Aggregates and returns the total VAT, available to everyone
     function totalVat() public view returns (uint256) {
         return (gatheredVat[0] + gatheredVat[1] + gatheredVat[2]);
     }
 
+    /*
+    Returns tax ID of the recipient with the most proceeds along with
+    their address and the amount of funds transferred to them. Only
+    available to the owner of the contract enforced by the onlyOwner
+    modifier.
+    */
     function getMaxProceedsPerson()
         public
         view
         onlyOwner
         returns (
-            uint256,
-            address,
-            uint256
+            uint256 TaxID,
+            address Address,
+            uint256 ProceedsForID
         )
     {
         return (
@@ -156,14 +238,17 @@ contract MyContract {
         );
     }
 
+    // Checks if the VAT level is in bounds (0,1,2)
     function checkIndexValidity(uint8 index) public pure returns (bool) {
         return (index == 0 || index == 1 || index == 2);
     }
 
+    // Returns the balance of address
     function checkBalance(address addr) public view returns (uint256) {
         return addr.balance;
     }
 
+    // Destroys the contract (permitted only by Owner)
     function destroy() public onlyOwner {
         selfdestruct(payable(owner));
     }
